4ec0cb3fcd4544c6f94b7d7b3efabb7061349234
zh33nbekov <rai.zheenbekov@gmail.com>
2025-03-14 16:27:47 +0600

    deleted unnecessary string

--- apps/client/src/components/header/Header.tsx
+++ apps/client/src/components/header/Header.tsx
@@ -149,7 +149,7 @@ export const Header = () => {
 									</li>
 								))}
 							</ul>
-							{/* <MyAdress /> */}src/modules/chat/components/chat/Chat.tsx
+							{/* <MyAdress /> */}
 						</nav>
 					</HeaderDrawer>
 				</>
