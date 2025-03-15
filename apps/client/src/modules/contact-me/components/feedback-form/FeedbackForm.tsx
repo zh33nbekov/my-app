1c24f48c44c4b622656a80f6efd16b26b6edacf8
zh33nbekov <rai.zheenbekov@gmail.com>
2025-03-15 22:33:10 +0600

    main refactor

--- apps/client/src/modules/contact-me/components/feedback-form/FeedbackForm.tsx
+++ apps/client/src/modules/contact-me/components/feedback-form/FeedbackForm.tsx
@@ -3,12 +3,14 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Input, showSuccessToast, Textarea } from '@packages/shared'
 import { useTranslations } from 'next-intl'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { FEEDBACK_FORM_INPUTS } from '../../constants'
 import { feedbackSchema, FeedbackShema, sendFeedback } from '../../index'
 import styles from './feedback-form.module.scss'
 
 export const FeedbackForm: React.FC = () => {
+	const [isLoading, setIsLoading] = useState(false)
 	const t = useTranslations('FeedbackForm')
 	const {
 		register,
@@ -22,9 +24,10 @@ export const FeedbackForm: React.FC = () => {
 
 	const submitHandler = async (data: FeedbackShema) => {
 		try {
-			const response = await sendFeedback(data)
-			console.log(response)
-			showSuccessToast(response.info)
+			setIsLoading(true)
+			const { info } = (await sendFeedback(data)) ?? { info: 'Something went wrong' }
+			setIsLoading(false)
+			showSuccessToast(info)
 			reset()
 		} catch (error) {
 			console.log(error)
@@ -51,7 +54,7 @@ export const FeedbackForm: React.FC = () => {
 				error={errors['message']?.message}
 				className={styles.feedbackForm__textarea}
 			/>
-			<Button className={styles.feedbackForm__submit} type='submit'>
+			<Button className={styles.feedbackForm__submit} type='submit' isLoading={isLoading}>
 				{t('send')}
 			</Button>
 		</form>
--- packages/shared/src/components/UI/button/Button.tsx
+++ packages/shared/src/components/UI/button/Button.tsx
@@ -1,14 +1,18 @@
 import { memo } from 'react'
+import { Loader } from '../loader/Loader'
 import styles from './button.module.scss'
 
 interface IButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 	active?: boolean
+	isLoading?: boolean
 }
 
-export const Button: React.FC<IButton> = memo(({ active, children, className, ...props }) => (
-	<button className={`${styles.button} ${className} ${active && styles.active}`} {...props}>
-		{children}
-	</button>
-))
+export const Button: React.FC<IButton> = memo(
+	({ active, children, className, isLoading, ...props }) => (
+		<button className={`${styles.button} ${className} ${active && styles.active}`} {...props}>
+			{isLoading ? <Loader /> : children}
+		</button>
+	)
+)
 
 Button.displayName = 'Button'
