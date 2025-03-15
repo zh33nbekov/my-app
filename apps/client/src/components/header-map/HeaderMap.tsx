1cc415e60265f237d7da108fc5585dbeabb554b8
zh33nbekov <rai.zheenbekov@gmail.com>
2025-03-15 21:16:59 +0600

    fixed headerMap loader

--- apps/client/src/components/header-map/HeaderMap.tsx
+++ apps/client/src/components/header-map/HeaderMap.tsx
@@ -1,4 +1,6 @@
 'use client'
+
+import clsx from 'clsx'
 import styles from './header-map.module.scss'
 
 import { useState } from 'react'
@@ -6,22 +8,23 @@ import { useState } from 'react'
 export const HeaderMap = () => {
 	const [isLoaded, setIsLoaded] = useState(false)
 	const handleLoad = () => setIsLoaded(true)
+	const handleTransitionEnd = (e: React.TransitionEvent<HTMLDivElement>) => {
+		e.currentTarget.style.display = 'none'
+	}
 
 	return (
 		<div className={styles.headerMap}>
 			<div
-				className={styles.headerMap__loader}
-				style={{
-					opacity: !isLoaded ? '1' : '0',
-				}}
+				onTransitionEnd={handleTransitionEnd}
+				className={clsx(styles.headerMap__loader, { [styles.disabled]: isLoaded })}
 			/>
 			<iframe
 				src='https://yandex.com/map-widget/v1/?ll=74.638673%2C42.849312&mode=search&ol=geo&ouri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgozMTQxNTk4NzEzEjzQmtGL0YDQs9GL0LfRgdGC0LDQvSwg0JHQuNGI0LrQtdC6LCDQotC-0qMg0LrTqdGH06nRgdKvLCAxMjQiCg0AR5VCFbJlK0I%2C&z=16.85'
 				allowFullScreen
 				style={{
-					opacity: isLoaded ? '1' : '0',
-					position: 'relative',
 					border: 'none',
+					position: 'relative',
+					opacity: isLoaded ? '1' : '0',
 				}}
 				onLoad={handleLoad}
 			></iframe>
