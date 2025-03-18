451141e388f4b185069835bde89c4aa3a6b77957
zh33nbekov <rai.zheenbekov@gmail.com>
2025-03-18 18:17:23 +0600

    refactor admin middleware

--- apps/admin/src/middleware.ts
+++ apps/admin/src/middleware.ts
@@ -1,21 +1,38 @@
-import { NextRequest } from 'next/server'
-import { authMiddleware } from './middlewares/auth.middleware'
-import { middlewareChain } from './middlewares/middleware.chain'
+// import { NextRequest } from 'next/server'
+// import { authMiddleware } from './middlewares/auth.middleware'
+// import { middlewareChain } from './middlewares/middleware.chain'
 
-const middlewares = [authMiddleware]
+// const middlewares = [authMiddleware]
 
-export const middleware = async (req: NextRequest) => await middlewareChain(req, middlewares)
+// export const middleware = async (req: NextRequest) => await middlewareChain(req, middlewares)
 
-// const cookie = await cookies()
-// const accessToken = cookie.get('accessToken')
-// console.log(accessToken, 'accessToken')
+// // const cookie = await cookies()
+// // const accessToken = cookie.get('accessToken')
+// // console.log(accessToken, 'accessToken')
 
-// const isAuthenticated = true
+// // const isAuthenticated = true
 
-// if (!isAuthenticated) {
-// 	return NextResponse.redirect(new URL('/login', req.url))
+// // if (!isAuthenticated) {
+// // 	return NextResponse.redirect(new URL('/login', req.url))
+// // }
+// // return NextResponse.next()
+
+// export const config = {
+// 	matcher: ['/dashboard/:path*'],
 // }
-// return NextResponse.next()
+
+import { cookies } from 'next/headers'
+import { NextRequest, NextResponse } from 'next/server'
+
+export const middleware = async (req: NextRequest) => {
+	const cookie = await cookies()
+	const accessToken = cookie.get('accessToken')
+
+	if (!accessToken) {
+		return NextResponse.redirect(new URL('/login', req.url))
+	}
+	return NextResponse.next()
+}
 
 export const config = {
 	matcher: ['/dashboard/:path*'],
--- apps/admin/src/middlewares/auth.middleware.ts
+++ apps/admin/src/middlewares/auth.middleware.ts
@@ -1,10 +1,9 @@
-// import { cookies } from 'next/headers'
+import { cookies } from 'next/headers'
 import { NextRequest, NextResponse } from 'next/server'
 
 export const authMiddleware = async (req: NextRequest) => {
-	// const cookie = await cookies()
-	// const accessToken = cookie.get('accessToken')
-	const accessToken = req.cookies.get('accessToken')
+	const cookie = await cookies()
+	const accessToken = cookie.get('accessToken')
 
 	if (!accessToken) {
 		return NextResponse.redirect(new URL('/login', req.url))
