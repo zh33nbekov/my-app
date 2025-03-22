89aeaeeb46f3a6c297f7299d4a9d21ff3d4b134b
zh33nbekov <rai.zheenbekov@gmail.com>
2025-03-22 19:06:01 +0600

    fixed my-app

--- apps/client/src/modules/skills/index.ts
+++ apps/client/src/modules/skills/index.ts
@@ -1,5 +1,3 @@
-export * from './components/skill-item/SkillItem'
-export * from './components/skills-category/SkillsCategory'
 export * from './components/skills/Skills'
 export * from './constants/skills'
 export * from './constants/skills-category'
